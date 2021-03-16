dd222ecbe05a5cfff3242c3775408dd1380b9067
Ulises Himely <47704656+ulises-codes@users.noreply.github.com>
2021-03-16 09:43:02 -0400

    Update function to remove whitespaces

--- lib/generateSitemap.ts
+++ lib/generateSitemap.ts
@@ -22,18 +22,15 @@ import fs from 'fs'
 
   const paths = routes.concat(posts)
 
-  const sitemap = `
-<?xml version="1.0" encoding="UTF-8"?>
-
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${paths
-  .map(
-    slug => `
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">${paths
+    .map(
+      slug => `
     <url>
         <loc>https://ulises.codes${slug}</loc>
     </url>`
-  )
-  .join('')}
+    )
+    .join('')}
 </urlset>
 `
 
