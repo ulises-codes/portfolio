0252c498ae0e94c2b7f65d7c6a73cf57b853cdef
Ulises Himely <47704656+ulises-codes@users.noreply.github.com>
2021-04-21 16:03:03 -0400

    Recursively create webmanifest directory

--- lib/mergeWebmanifest.ts
+++ lib/mergeWebmanifest.ts
@@ -6,9 +6,14 @@ import main from '../main.manifest.json'
 
 const rootDir = join(__dirname, '../')
 
+const dirPath = `${rootDir}/public/webmanifest`
+
+fs.mkdirSync(dirPath, { recursive: true })
+
 themeList.forEach(({ name, background_color, theme_color }) => {
   fs.writeFileSync(
-    `${rootDir}/public/webmanifest/${name}.webmanifest`,
-    JSON.stringify({ ...main, background_color, theme_color })
+    `${dirPath}/${name}.webmanifest`,
+    JSON.stringify({ ...main, background_color, theme_color }),
+    { flag: 'w' }
   )
 })
