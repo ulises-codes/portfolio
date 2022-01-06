b19148d24395855af39833817696553be9c9b894
Ulises Himely <47704656+ulises-codes@users.noreply.github.com>
2022-01-06 16:52:22 -0500

    Add GA Tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,7 @@ import 'lib/register'
 import { useEffect, useState } from 'react'
 
 import dynamic from 'next/dynamic'
-
+import Script from 'next/script'
 import { useRouter } from 'next/router'
 
 import { LazyMotion } from 'framer-motion'
@@ -87,23 +87,44 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, [isOnline, router.route])
 
   return (
-    <Layout
-      theme={{
-        currentTheme,
-        setCurrentTheme: theme => setCurrentTheme(theme),
-      }}
-    >
-      {router.pathname === '/' ? (
-        <LazyMotion
-          features={() =>
-            import('../util/motion-features').then(mod => mod.default)
-          }
-        >
+    <>
+      <Script
+        src="https://www.google-analytics.com/analytics.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+
+          function gtag(){
+              dataLayer.push(arguments);
+            }
+
+            gtag('js', new Date());
+
+            gtag('config', 'G-M6ZF536ND5');`,
+        }}
+      />
+      <Layout
+        theme={{
+          currentTheme,
+          setCurrentTheme: theme => setCurrentTheme(theme),
+        }}
+      >
+        {router.pathname === '/' ? (
+          <LazyMotion
+            features={() =>
+              import('../util/motion-features').then(mod => mod.default)
+            }
+          >
+            <Component {...pageProps} theme={currentTheme} />
+          </LazyMotion>
+        ) : (
           <Component {...pageProps} theme={currentTheme} />
-        </LazyMotion>
-      ) : (
-        <Component {...pageProps} theme={currentTheme} />
-      )}
-    </Layout>
+        )}
+      </Layout>
+    </>
   )
 }
