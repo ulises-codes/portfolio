97b8ab9e478eee4e5f77fc5f886859771e68bfe7
Ulises Himely <47704656+ulises-codes@users.noreply.github.com>
2022-01-12 13:20:17 -0500

    Update cli tool to switch statement and change argument from 'post' to 'create'

--- cli/index.js
+++ cli/index.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
-'use strict'
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod }
-  }
-
-exports.__esModule = true
-var newPost_1 = __importDefault(require('./lib/newPost'))
-var args = process.argv.slice(2)
-if (args[0] === 'post' && !!args[1]) {
-  ;(0, newPost_1['default'])(args[1])
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var newPost_1 = __importDefault(require("./lib/newPost"));
+var args = process.argv.slice(2);
+switch (args[0]) {
+    case 'create':
+        (0, newPost_1["default"])(args[1]);
+        break;
 }
--- cli/index.ts
+++ cli/index.ts
@@ -3,7 +3,9 @@ import newBlogPost from './lib/newPost'
 
 const args = process.argv.slice(2)
 
-if (args[0] === 'post' && !!args[1]) {
-  newBlogPost(args[1])
-}
+switch (args[0]) {
+  case 'create':
+    newBlogPost(args[1])
 
+    break
+}
