1e3715fd4184f7f940c89603d0d4acb76674b0f0
Ulises Himely <>
2023-06-16 18:46:07 -0400

    remove certifications seciton

--- components/HomePage/Bio/TLDR.tsx
+++ components/HomePage/Bio/TLDR.tsx
@@ -17,22 +17,6 @@ export default function TLDR() {
         , that guy on the $50 bill. I code in JavaScript, Python, React,
         Typescript, HTML, and CSS.
       </p>
-      <div>
-        <h4 className='subtitle'>Certifications</h4>
-        <a
-          href='https://www.credly.com/badges/a089e4e1-db25-41d4-8948-e6ea4b3bd804/public_url'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            src='/ulises.codes/certifications/aws-ccp.png'
-            height='60'
-            width='60'
-            alt=''
-            role='presentation'
-          />
-        </a>
-      </div>
     </div>
   );
 }
