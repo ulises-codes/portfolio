f33ef3db60dd6a5c847eb287785943f0b0864746
Ulises Himely <>
2023-06-16 18:35:15 -0400

    fix build errors

--- components/HomePage/Bio/TLDR.tsx
+++ components/HomePage/Bio/TLDR.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function TLDR() {
   return (
@@ -7,10 +7,10 @@ export default function TLDR() {
         My name is Ulises Himely. Species: human. My name is pronounced the same
         as{' '}
         <a
-          className="link"
-          href="https://en.wikipedia.org/wiki/Ulysses_S._Grant"
-          rel="norefferer noopener"
-          target="_blank"
+          className='link'
+          href='https://en.wikipedia.org/wiki/Ulysses_S._Grant'
+          rel='norefferer noopener'
+          target='_blank'
         >
           Ulysses S. Grant
         </a>
@@ -18,19 +18,21 @@ export default function TLDR() {
         Typescript, HTML, and CSS.
       </p>
       <div>
-        <h4 className="subtitle">Certifications</h4>
+        <h4 className='subtitle'>Certifications</h4>
         <a
-          href="https://www.credly.com/badges/a089e4e1-db25-41d4-8948-e6ea4b3bd804/public_url"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.credly.com/badges/a089e4e1-db25-41d4-8948-e6ea4b3bd804/public_url'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
-            src="/ulises.codes/certifications/aws-ccp.png"
-            height="60"
-            width="60"
+            src='/ulises.codes/certifications/aws-ccp.png'
+            height='60'
+            width='60'
+            alt=''
+            role='presentation'
           />
         </a>
       </div>
     </div>
-  )
+  );
 }
--- components/HomePage/Portfolio/Projects/Thumbnail.tsx
+++ components/HomePage/Portfolio/Projects/Thumbnail.tsx
@@ -1,8 +1,8 @@
-import { ProjectProps } from 'interfaces'
+import { ProjectProps } from 'interfaces';
 
-import styles from './styles.module.scss'
-import MyImage from 'util/MyImage'
-import { imgToBase64 } from 'lib/helper/imgToBase64'
+import styles from './styles.module.scss';
+import MyImage from 'util/MyImage';
+import { imgToBase64 } from 'lib/helper/imgToBase64';
 
 export default function Thumbnail({
   name,
@@ -12,33 +12,35 @@ export default function Thumbnail({
 }: ProjectProps) {
   return (
     <div className={styles['thumbnail-root--div']}>
-      <a href={`https://${url}`} target="_blank" rel="noopener noreferrer">
+      <a href={`https://${url}`} target='_blank' rel='noopener noreferrer'>
         <span>{name}</span>
         <MyImage
+          alt=''
+          role='presentation'
           src={`f_auto/ulises.codes/project-thumbnails/${imgSrc}`}
-          width="275"
-          height="175"
-          objectFit="cover"
-          placeholder="blur"
+          width='275'
+          height='175'
+          objectFit='cover'
+          placeholder='blur'
           blurDataURL={
             imgToBase64(
-              `${process.env.NEXT_PUBLIC_CLOUDINARY_PREFIX}/image/upload/c_scale,e_blur:102,w_300/ulises.codes/project-thumbnails/${imgSrc}`
+              `${process.env.NEXT_PUBLIC_CLOUDINARY_PREFIX}/image/upload/c_scale,e_blur:102,w_300/ulises.codes/project-thumbnails/${imgSrc}`,
             ) as unknown as string
           }
         />
       </a>
       <div className={styles['thumbnail-details-languages--div']}>
-        {languages.map(language => (
+        {languages.map((language) => (
           <img
             key={`${name}-language-badge-${language}`}
-            className="language-logo-badge--img"
+            className='language-logo-badge--img'
             src={`/images/language-logos/${language.toLocaleLowerCase()}.svg`}
-            height="24"
-            width="24"
+            height='24'
+            width='24'
             title={language}
           />
         ))}
       </div>
     </div>
-  )
+  );
 }
